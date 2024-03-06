dbd3092e9e4a65993c6e28841fe2681af2d82cb8
Lucasfransa053 <lucasfransa053@gmail.com>
2024-03-06 18:21:26 -0300

    fibonacci

---
+++ target.py
@@ -0,0 +1,18 @@
+def verifica_fibonacci(numero):
+    
+    a, b = 0, 1
+    while b < numero:
+        a, b = b, a + b
+    if b == numero:
+        return True
+    else:
+        return False
+while True:
+    numero_informado = int(input("Digite um número: "))
+
+    if verifica_fibonacci(numero_informado):
+        print(f'O número {numero_informado} pertence à sequência de Fibonacci.')
+    else:
+        print(f'O número {numero_informado} não pertence à sequência de Fibonacci.')
+    print('---------')
+    
